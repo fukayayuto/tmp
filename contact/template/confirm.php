6d3e8af4916493bb77d4560b0c89e81b4f008b5b
Akihiro.TOYOSHIMA <cabstation_ccd@kabushikikaishakyabusuteshonnoMacBook-Air.local>
2022-01-13 14:53:49 +0900

    change_ficxffjdjnjvndj

--- contact/template/confirm.php
+++ contact/template/confirm.php
@@ -100,19 +100,6 @@
   </main>
   <!-- //main --> 
   <!-- footer// -->
-  <footer>
-    <div id="footIn">
-      <ul>
-        <li><a href="https://cab-station.com/company.html" target="_blank">運営会社</a></li>
-        <li><a href="/contact/">お問合せ</a></li>
-        <li><a href="https://cab-station.com/travel.html" target="_blank">インターネット旅行取引の詳細</a></li>
-        <li><a href="https://cab-station.com/stipulation.html" target="_blank">旅行業約款</a></li>
-        <li><a href="/about/term.html">取引条件書</a></li>
-        <li><a href="https://cab-station.com/privacy.html" target="_blank">個人情報保護方針</a></li>
-      </ul>
-      <p>© 2021 Cab Station Co., Ltd.</p>
-    </div>
-  </footer>
   <?php include($_SERVER['DOCUMENT_ROOT']."/common/inc/base_footer.inc")?>
   <!-- //footer --> 
 </div>
